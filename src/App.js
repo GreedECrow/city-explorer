b194b0e685aa3d200f90c7e6fc7fa2470c57da60
Lee Pierson <awesomehenge@gmail.com>
2023-07-28 14:44:36 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function app(){
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+  const [map, setMap] = useState("");
+
+  function handleSearch(event) {
+    setSearchQuery(event.target.value);
+  }
 }
+
+async function handleSearch(event) {
+
+  try{
+    event.preventDefault();
+    event.target.input.value = "";
+    const API = `https://us1.locationiq.com/v1/search.php=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+    const res = await axios.get(API);
+    setLocation(res.data[0]);
+    handleMap(res.data[0]);
+  } catch (error) {
+    console.log (error);
+  }
+ }
+
+
+ async function handleSearch(event) {
+
+  try{
+    event.preventDefault();
+    event.target.input.value = "";
+    const API = `https://eu1.locationiq.com/v1/search.php${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+    const res = await axios.get(API);
+    setLocation(res.data[0]);
+    handleMap(res.data[0]);
+  } catch (error) {
+    console.log (error);
+  }
+ }
+
+
+ function handleMap(data) {
+  const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=9`;
+  setMap(API);
+ }
+
+ return (
+  <div>
+    <form>
+      <input type="text" placeholder="Search for a place" onChange={handleSearch} name="input"></input>
+      <button type="Submit">Explore!</button> 
+    </form>
+  </div>
+ )
+
 
 export default App;
